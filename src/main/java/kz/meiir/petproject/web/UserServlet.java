7ba66661e477aaafb0f9238e6e7ce4b8d34bd499
Meiir Akhmetov HP <meir.akhmetov@gmail.com>
2023-01-06 15:30:04 +0600

    1_7_logging

--- src/main/java/kz/meiir/petproject/web/UserServlet.java
+++ src/main/java/kz/meiir/petproject/web/UserServlet.java
@@ -1,19 +1,26 @@
 package kz.meiir.petproject.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * @author Meiir Akhmetov on 05.01.2023
  */
 public class UserServlet extends HttpServlet {
+    private static final Logger Log  = getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        request.getRequestDispatcher("/users.jsp").forward(request,response);
+        Log.debug("redirect tot users");
+//        request.getRequestDispatcher("/users.jsp").forward(request,response);
+        response.sendRedirect("users.jsp");
     }
 
 }
