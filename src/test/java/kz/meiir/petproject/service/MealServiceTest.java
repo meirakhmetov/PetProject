763edc2c441b463c2522836be799d8069a45edd1
Meiir Akhmetov HP <meir.akhmetov@gmail.com>
2023-01-25 16:51:27 +0600

    5_4_HW4_optional

--- src/test/java/kz/meiir/petproject/service/MealServiceTest.java
+++ src/test/java/kz/meiir/petproject/service/MealServiceTest.java
@@ -2,8 +2,14 @@ package kz.meiir.petproject.service;
 
 import kz.meiir.petproject.model.Meal;
 import kz.meiir.petproject.util.exception.NotFoundException;
+import org.junit.AfterClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,11 +19,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.concurrent.TimeUnit;
 
 import static kz.meiir.petproject.MealTestData.*;
 import static kz.meiir.petproject.UserTestData.ADMIN_ID;
 import static kz.meiir.petproject.UserTestData.USER_ID;
 import static org.junit.Assert.*;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * @author Meiir Akhmetov on 20.01.2023
@@ -29,22 +37,51 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
+    private static final Logger Log = getLogger(MealServiceTest.class);
+
+    private static StringBuilder results = new StringBuilder();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Rule
+    //http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description){
+            String result = String.format("\n%-25s %8d", description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos));
+            results.append(result);
+            Log.info(result+" ms\n");
+        }
+    };
+
+    @AfterClass
+    public static void printResult(){
+        Log.info("\n--------------------------" +
+                "\nTest          Duration, ms" +
+                "\n--------------------------" +
+                results +
+                "\n--------------------------");
+    }
 
     @Autowired
     private MealService service;
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void delete() throws Exception{
         service.delete(MEAL1_ID, USER_ID);
+        thrown.expect(NotFoundException.class);
         service.get(MEAL1_ID,USER_ID);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void deleteNotFound() throws Exception{
+        thrown.expect(NotFoundException.class);
         service.delete(1,USER_ID);
     }
-    @Test(expected = NotFoundException.class)
+    @Test
     public void deleteNotOwn() throws Exception{
+        thrown.expect(NotFoundException.class);
         service.delete(MEAL1_ID,ADMIN_ID);
     }
 
@@ -64,13 +101,15 @@ public class MealServiceTest {
         assertMatch(actual,ADMIN_MEAL1);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void getNotFound() throws Exception{
+        thrown.expect(NotFoundException.class);
         service.get(1,USER_ID);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void getNotOwn() throws Exception{
+        thrown.expect(NotFoundException.class);
         service.get(MEAL1_ID,ADMIN_ID);
     }
 
@@ -81,8 +120,10 @@ public class MealServiceTest {
         assertMatch(service.get(MEAL1_ID,USER_ID),updated);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void updateNotFound() throws Exception{
+        thrown.expect(NotFoundException.class);
+        thrown.expectMessage("Not found entity with id=" + MEAL1_ID);
         service.update(MEAL1,ADMIN_ID);
     }
 
