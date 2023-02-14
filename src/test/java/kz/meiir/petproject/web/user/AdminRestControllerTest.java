7b799bbff7f60ba52c0d6e78dea9976ce49d8590
Meiir Akhmetov HP <meir.akhmetov@gmail.com>
2023-02-14 16:44:42 +0600

    7_10_rest_test_jackson

---
+++ src/test/java/kz/meiir/petproject/web/user/AdminRestControllerTest.java
@@ -0,0 +1,28 @@
+package kz.meiir.petproject.web.user;
+
+import kz.meiir.petproject.web.AbstractControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static kz.meiir.petproject.UserTestData.ADMIN_ID;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Meiir Akhmetov on 14.02.2023
+ */
+class AdminRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    @Test
+    void get() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(REST_URL + ADMIN_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+}
