e73ba7e59075e82251576d11eb7f8db483331188
Meiir Akhmetov HP <meir.akhmetov@gmail.com>
2023-02-14 15:27:12 +0600

    7_09_rest_controller

--- src/main/java/kz/meiir/petproject/web/user/ProfileRestController.java
+++ src/main/java/kz/meiir/petproject/web/user/ProfileRestController.java
@@ -1,25 +1,35 @@
 package kz.meiir.petproject.web.user;
 
 import kz.meiir.petproject.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import static kz.meiir.petproject.web.SecurityUtil.authUserId;
 
 /**
  * @author Meiir Akhmetov on 10.01.2023
  */
-@Controller
+@RestController
+@RequestMapping(ProfileRestController.REST_URL)
 public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get(){
         return super.get(authUserId());
     }
 
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(){
         super.delete(authUserId());
     }
 
-    public void update(User user){
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user){
         super.update(user, authUserId());
     }
 }
