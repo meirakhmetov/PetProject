5731de869da740b1c58d3c5bf81a63dfaefef8a3
Meiir Akhmetov HP <meir.akhmetov@gmail.com>
2023-03-10 14:27:59 +0600

    10_05_opt_validated_groups

--- src/main/java/kz/meiir/petproject/web/meal/MealUIController.java
+++ src/main/java/kz/meiir/petproject/web/meal/MealUIController.java
@@ -5,13 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import kz.meiir.petproject.View;
+import kz.meiir.petproject.View.ValidatedUI;
 import kz.meiir.petproject.model.Meal;
 import kz.meiir.petproject.to.MealTo;
 import kz.meiir.petproject.util.ValidationUtil;
 
-import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -46,7 +47,7 @@ public class MealUIController extends AbstractMealController{
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public ResponseEntity<String> createOrUpdate(@Valid Meal meal, BindingResult result){
+    public ResponseEntity<String> createOrUpdate(@Validated(ValidatedUI.class) Meal meal, BindingResult result){
         if(result.hasErrors()){
             return ValidationUtil.getErrorResponse(result);
         }
